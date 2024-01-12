1bcba5196c050de676cbbdfed150e2b8625fd10b
abhi4450 <abhishek.anshu1991>
2024-01-12 13:14:07 +0530

    added delete functionality

--- models/cart.js
+++ models/cart.js
@@ -25,7 +25,7 @@ module.exports = class Cart {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
-        cart.products[existingProductIndex] = existingProduct;
+        cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
