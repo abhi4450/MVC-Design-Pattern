805380dd5c01b6872001cd15ca61b8bf1f7010b2
abhi4450 <abhishek.anshu1991>
2024-03-25 15:40:36 +0530

    implemented all crud functionality using mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const product = require("../models/product");
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -59,15 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      (product.title = updatedTitle),
+        (product.price = updatedPrice),
+        (product.description = updatedDesc),
+        (product.imageUrl = updatedImageUrl);
+      return product.save();
+    })
     .then((result) => {
       console.log("updated succesfully");
       res.redirect("/admin/products");
@@ -76,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -89,9 +89,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findOneAndDelete({ _id: prodId })
+
     .then((result) => {
-      console.log("****deleted successfully*****");
+      console.log("product deleted successfully");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
